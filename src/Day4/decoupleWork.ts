d73757babb7f44756facc728306bf76e85c2f821
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2022-12-07 17:33:23 -0600

    day 4 part one

--- src/Day4/decoupleWork.ts
+++ src/Day4/decoupleWork.ts
@@ -5,6 +5,26 @@ export const decoupleWork = (path: PathOrFileDescriptor) => {
     .readFileSync(path, { encoding: "utf-8" })
     .split(/\n/)
     .map((elm) => elm.split(","));
+  sectionAssignmentList.forEach((elm) => OverLap.Find(elm));
+  console.log(OverLap.foundContains);
+};
 
-  console.log(sectionAssignmentList);
+const OverLap = {
+  foundContains: 0,
+  foundOverLaps: 0,
+  Find(sectionPair) {
+    const [aPair, bPair] = sectionPair;
+    const [aLow, aHigh] = aPair.split("-");
+    const [bLow, bHigh] = bPair.split("-");
+    const isOverLap = +bHigh - +aLow > -1 && +bLow - +aHigh < 1;
+    const isContains =
+      (+aHigh >= +bHigh && +aLow <= +bLow) ||
+      (+bHigh >= +aHigh && +bLow <= +aLow);
+    if (isOverLap) {
+      if (isContains) {
+        this.foundContains++;
+      }
+      this.foundOverLaps++;
+    }
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,38 @@ import { CaloriesCount } from "./Day1/CaloriesCount.js";
 import { RPSScoreCard } from "./Day2/RPSScoreCard.js";
 import { decoupleWork } from "./Day4/decoupleWork.js";
 
+const sample = 1;
 const Day: number = 4;
 const dataPath = "./src/data/";
 
-// CaloriesCount(`${dataPath}/ElvesCount.txt`);
-switch (Day) {
-  case 1:
-    CaloriesCount(`${dataPath}/ElvesCount.txt`);
-    break;
-  case 2:
-    RPSScoreCard(`${dataPath}/RPSGuide.txt`);
-    break;
-  case 3:
-    SortSack(`${dataPath}/SackList.txt`);
-    break;
-  case 4:
-    decoupleWork(`${dataPath}/SectionAssignmentPair.txt`);
-    break;
+if (sample) {
+  switch (Day) {
+    case 1:
+      CaloriesCount(`${dataPath}/ElvesCount.txt`);
+      break;
+    case 2:
+      RPSScoreCard(`${dataPath}/RPSGuide.txt`);
+      break;
+    case 3:
+      SortSack(`${dataPath}/SackList.txt`);
+      break;
+    case 4:
+      decoupleWork(`${dataPath}/SectionAssignmentPair.txt`);
+      break;
+  }
+} else {
+  switch (Day) {
+    case 1:
+      CaloriesCount(`${dataPath}/ElvesCount.txt`);
+      break;
+    case 2:
+      RPSScoreCard(`${dataPath}/RPSGuide.txt`);
+      break;
+    case 3:
+      SortSack(`${dataPath}/SackList.txt`);
+      break;
+    case 4:
+      decoupleWork(`${dataPath}/sampleSectionAssignmentPair.txt`);
+      break;
+  }
 }
