ab7e903299c0fbcf22941182e5820f302539a21b
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2022-12-07 09:01:36 -0600

    Day 4 settup

--- src/Day4/decoupleWork.ts
+++ src/Day4/decoupleWork.ts
@@ -1,3 +1,10 @@
 import fs, { PathOrFileDescriptor } from "fs";
 
-export const decoupleWork = (path: PathOrFileDescriptor) => {};
+export const decoupleWork = (path: PathOrFileDescriptor) => {
+  const sectionAssignmentList = fs
+    .readFileSync(path, { encoding: "utf-8" })
+    .split(/\n/)
+    .map((elm) => elm.split(","));
+
+  console.log(sectionAssignmentList);
+};
