d880831cd2ef483ccb81c3fb461b29950b77d523
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2022-12-07 08:58:31 -0600

    js switch cases are dumb

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,21 @@ import { CaloriesCount } from "./Day1/CaloriesCount.js";
 import { RPSScoreCard } from "./Day2/RPSScoreCard.js";
 import { decoupleWork } from "./Day4/decoupleWork.js";
 
-const Day: number = 1;
+const Day: number = 4;
 const dataPath = "./src/data/";
 
-CaloriesCount(`${dataPath}/ElvesCount.txt`);
-// switch (Day) {
-//   case 1:
-//     CaloriesCount(`${dataPath}/ElvesCount.txt`);
-//   case 2:
-//     RPSScoreCard(`${dataPath}/RPSGuide.txt`);
-//   case 3:
-//     SortSack(`${dataPath}/SackList.txt`);
-//   case 4:
-//     decoupleWork(`${dataPath}/SectionAssignmentPair.txt`);
-// }
+// CaloriesCount(`${dataPath}/ElvesCount.txt`);
+switch (Day) {
+  case 1:
+    CaloriesCount(`${dataPath}/ElvesCount.txt`);
+    break;
+  case 2:
+    RPSScoreCard(`${dataPath}/RPSGuide.txt`);
+    break;
+  case 3:
+    SortSack(`${dataPath}/SackList.txt`);
+    break;
+  case 4:
+    decoupleWork(`${dataPath}/SectionAssignmentPair.txt`);
+    break;
+}
