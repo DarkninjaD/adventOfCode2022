17b2359925f4d3f2ff0a59709c9364dd2aea6e01
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2022-12-08 15:43:35 -0600

    day 6 part two

--- src/Day6/ElfSignalDecoder.ts
+++ src/Day6/ElfSignalDecoder.ts
@@ -18,7 +18,36 @@ export const ElfSignalDecoder = (path: PathOrFileDescriptor) => {
     checkSet.add(signal.charAt(pointer + 3));
     if (checkSet.size == 4) {
       console.log(checkSet);
-      console.log("First marker after ", pointer + 4);
+      console.log(
+        "First marker for start-of-packet after character",
+        pointer + 4
+      );
+      pointer = signal.length;
+    }
+  }
+
+  for (let pointer = 0; pointer < signal.length; pointer++) {
+    const checkSet = new Set();
+    checkSet.add(signal.charAt(pointer));
+    checkSet.add(signal.charAt(pointer + 1));
+    checkSet.add(signal.charAt(pointer + 2));
+    checkSet.add(signal.charAt(pointer + 3));
+    checkSet.add(signal.charAt(pointer + 4));
+    checkSet.add(signal.charAt(pointer + 5));
+    checkSet.add(signal.charAt(pointer + 6));
+    checkSet.add(signal.charAt(pointer + 7));
+    checkSet.add(signal.charAt(pointer + 8));
+    checkSet.add(signal.charAt(pointer + 9));
+    checkSet.add(signal.charAt(pointer + 10));
+    checkSet.add(signal.charAt(pointer + 11));
+    checkSet.add(signal.charAt(pointer + 12));
+    checkSet.add(signal.charAt(pointer + 13));
+    if (checkSet.size == 14) {
+      console.log(checkSet);
+      console.log(
+        "First marker for start-of-message after character",
+        pointer + 14
+      );
       pointer = signal.length;
     }
   }
